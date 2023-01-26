40dee5a18e398e4a7e98c5803dcefecb1857e4db
Aa-thomas <aarthomas01@gmail.com>
2023-01-25 21:39:56 -0500

    feat: add serverside validation when user is created

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 // Construct a router instance.
 const router = express.Router();
@@ -32,8 +33,47 @@ router.get(
 router.post(
 	'/users',
 	asyncHandler(async (req, res) => {
-		await User.create(req.body);
-		res.redirect('/users');
+		// Get the user from the request body.
+		const user = req.body;
+		console.log('email-------->', user.firstName, user.lastName);
+
+		// Store errors
+		const errors = [];
+
+		// Validate that we have a `firstName` value.
+		if (!user.firstName) {
+			errors.push('Please provide a first name');
+		}
+
+		// Validate that we have a `lastName` value.
+		if (!user.lastName) {
+			errors.push('Please provide a last name');
+		}
+
+		// Validate that we have an `email` value.
+		if (!user.email) {
+			errors.push('Please provide a value for "email"');
+		}
+
+		// Validate that we have a `password` value.
+		let password = user.password;
+		if (!password) {
+			errors.push('Please provide a value for "password"');
+		} else if (password.length < 8 || password.length > 20) {
+			errors.push('Your password should be between 8 and 20 characters');
+		}
+
+		// If there are any errors...
+		if (errors.length > 0) {
+			// Return the validation errors to the client.
+			res.status(400).json({ errors });
+		} else {
+			// Add the user to the database.
+			await User.create(req.body);
+
+			// Set the status to 201 Created and end the response.
+			res.status(201).end();
+		}
 	})
 );
 
